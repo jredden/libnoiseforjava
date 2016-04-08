d872a5e6d69f5e637684ed955914e42b9239b74e
jredden <johnredden@aol.com>
2016-04-07 14:59:25 -1000

    lesson four: http://libnoise.sourceforge.net/tutorials/tutorial4.html

---
+++ src/main/java/libnoiseforjava/domain/Builder.java
@@ -0,0 +1,79 @@
+package libnoiseforjava.domain;
+
+import libnoiseforjava.exception.ExceptionInvalidParam;
+import libnoiseforjava.util.ImageCafe;
+import libnoiseforjava.util.NoiseMap;
+import libnoiseforjava.util.NoiseMapBuilderPlane;
+import libnoiseforjava.util.RendererImage;
+
+
+public class Builder {
+
+	/**
+	 * 
+	 * @param width
+	 * @param height
+	 * @return NoiseMap
+	 * @throws ExceptionInvalidParam
+	 */
+	public static NoiseMap createNoiseMap(Integer width, Integer height) throws ExceptionInvalidParam{
+		return new NoiseMap(width, height);
+	}
+	
+	/**
+	 * 
+	 * @param builderPlaneParameters
+	 * @return NoiseMapBuilderPlane
+	 * @throws ExceptionInvalidParam
+	 */
+	public static NoiseMapBuilderPlane builderPlane(
+			BuilderPlaneParameters builderPlaneParameters)
+			throws ExceptionInvalidParam {
+		// create Builder object
+		NoiseMapBuilderPlane noiseMapBuilderPlane = new NoiseMapBuilderPlane();
+		noiseMapBuilderPlane
+				.setSourceModule(builderPlaneParameters.getPerlin());
+		noiseMapBuilderPlane.setDestNoiseMap(builderPlaneParameters
+				.getNoiseMap());
+		noiseMapBuilderPlane.setDestSize(
+				builderPlaneParameters.getDestHeight(),
+				builderPlaneParameters.getDestWidth());
+		noiseMapBuilderPlane.setBounds(builderPlaneParameters.getLowerXBound(),
+				builderPlaneParameters.getUpperXBound(),
+				builderPlaneParameters.getLowerZBound(),
+				builderPlaneParameters.getUpperZBound());
+		noiseMapBuilderPlane.build();
+		return noiseMapBuilderPlane;
+	}
+	
+	/**
+	 * 
+	 * @param renderImageParameter
+	 * @return RenderImage
+	 * @throws ExceptionInvalidParam
+	 */
+	public static ImageCafe buildRendererImage(
+			RenderImageParameter renderImageParameter)
+			throws ExceptionInvalidParam {
+		RendererImage renderer = new RendererImage();
+		renderer.clearGradient();
+		for (GradientPointParameter gradientPointParameter : renderImageParameter
+				.getGradientPointList()) {
+			renderer.addGradientPoint(
+					gradientPointParameter.getGradientPosition(),
+					gradientPointParameter.getColorCafe());
+		}
+		ImageCafe imageCafe = new ImageCafe(renderImageParameter.getNoiseMap()
+				.getHeight(), renderImageParameter.getNoiseMap().getWidth());
+		renderer.setSourceNoiseMap(renderImageParameter.getNoiseMap());
+
+		renderer.setDestImage(imageCafe);
+		renderer.enableLight(renderImageParameter.getLightEnable());
+		renderer.setLightContrast(renderImageParameter.getLightContrast());
+		renderer.setLightBrightness(renderImageParameter.getLightBrightness());
+		renderer.render();
+
+		return imageCafe;
+	}
+
+}
