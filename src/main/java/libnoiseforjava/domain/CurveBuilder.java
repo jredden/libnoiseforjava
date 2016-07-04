9489f0bd311b95aa1a8704c3f5eb641b54a2739f
john redden <johnredden@aol.com>
2016-07-03 17:00:58 -1000

    experiments with perlin, builder modules

---
+++ src/main/java/libnoiseforjava/domain/CurveBuilder.java
@@ -0,0 +1,60 @@
+package libnoiseforjava.domain;
+
+import java.util.List;
+
+import libnoiseforjava.module.Curve;
+import libnoiseforjava.module.ModuleBase;
+
+public class CurveBuilder {
+	
+	class ControlPoint{
+		public Double inputValue;
+		public Double outputValue;
+	}
+	
+	ModuleBase moduleBase;
+	Double medianValue;
+	List<ControlPoint> controlPoints;
+	Curve curve;
+	
+	
+	public Curve builder(ModuleBase moduleBase, Double medianValue, List<ControlPoint> controlPoints) {
+		this.moduleBase = moduleBase;
+		this.medianValue = medianValue;
+		this.controlPoints = controlPoints;
+		this.curve = new Curve(moduleBase);
+		for(ControlPoint controlPoint : controlPoints){
+			this.curve.addControlPoint(controlPoint.inputValue + this.medianValue, controlPoint.outputValue + this.medianValue);
+		}
+		return this.curve;
+	}
+
+
+	public ModuleBase getModuleBase() {
+		return moduleBase;
+	}
+
+
+	public Double getMedianValue() {
+		return medianValue;
+	}
+
+
+	public List<ControlPoint> getControlPoints() {
+		return controlPoints;
+	}
+
+
+	public Curve getCurve() {
+		return curve;
+	}
+
+
+	@Override
+	public String toString() {
+		return "CurveBuilder [moduleBase=" + moduleBase + ", medianValue=" + medianValue + ", controlPoints="
+				+ controlPoints +"]";
+	}
+
+	
+}
