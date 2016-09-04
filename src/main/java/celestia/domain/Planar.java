6a3e4962c7ef905b3d76634b4b1b391a9204c9b3
john redden <johnredden@aol.com>
2016-09-03 17:51:07 -1000

    libnoise plus celestia 36

--- src/main/java/celestia/domain/Planar.java
+++ src/main/java/celestia/domain/Planar.java
@@ -1,6 +1,12 @@
 package celestia.domain;
 
+import com.zenred.cosmos.domain.Planetoid;
+import com.zenred.cosmos.domain.Star;
+
 public class Planar {
+	
+	public static enum PlanarClass {PLANET, MOON}
+	
 	/*
 	 * Open GL colors ... take HTML colors and divide by 255
 	 */
@@ -10,7 +16,7 @@ public class Planar {
 		Float bOfRGB;
 	}
 	
-	private String planarClass;
+	private PlanarClass planarClass;
 	private String texture;
 	private String nightTexture;
 	private OGL_Color color;
@@ -19,5 +25,26 @@ public class Planar {
 	private OGL_Color hazeColor;
 	private Float hazeDensity;
 	private Double radius;
-
+	private Double oblateness;
+	private Integer atmosphereHeight;
+	private OGL_Color lower;
+	private OGL_Color upper;
+	private OGL_Color sky;
+	private Integer cloudHeight;
+	private String cloudMap;
+	private Integer cloudSpeed;
+	private Double period;
+	private Double semiMajorAxis;
+	private Double eccentricity;
+	private Double longOfPericenter;
+	private Double meanLongitude;
+	private Double rotationPeriod;
+	private Double obliquity;
+	private Double albedo;
+	
+	public static Planar build(Star star, Planetoid planetoid, PlanarClass planarClass){
+		Planar instance = new Planar();
+		
+		return instance;
+	}
 }
