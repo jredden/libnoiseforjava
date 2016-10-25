c6a4ee5123c23a1b5931f919c7ca36486264956e
john redden <johnredden@aol.com>
2016-10-24 18:05:04 -1000

    libnoise plus celestia 67

--- src/main/java/celestia/PlanarAlbedo.java
+++ src/main/java/celestia/PlanarAlbedo.java
@@ -2,14 +2,21 @@ package celestia;
 
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
+
+import com.zenred.cosmos.domain.Atmosphere;
+import com.zenred.cosmos.domain.ExistingSystemWithStars;
 import com.zenred.cosmos.domain.Star;
 import com.zenred.cosmos.domain.UnifiedPlanetoidI;
 import com.zenred.util.GenRandomRolls;
 
 public class PlanarAlbedo {
+	
+	private static Logger logger = Logger.getLogger(PlanarAlbedo.class);
 	
 	interface PlanarTemperature {
 		public Double modAlbedo(Double albedo);
@@ -28,30 +35,36 @@ public class PlanarAlbedo {
 		temperatureMap.put(new Double(0.0), new PlanarTemperature() {
 			@Override
 			public Double modAlbedo(Double albedo) {
+				logger.info("start albedo:"+albedo);
 				albedo += GenRandomRolls.Instance().draw_rand()*0.7;
 				if(albedo > 1.0){
 					albedo = 1.0 - GenRandomRolls.Instance().draw_rand() * 0.01;
 				}
+				logger.info("end albedo:"+albedo);
 				return albedo;
 			}
 		});
 		temperatureMap.put(new Double(100.0), new PlanarTemperature() {
 			@Override
 			public Double modAlbedo(Double albedo) {
+				logger.info("start albedo:"+albedo);
 				albedo += GenRandomRolls.Instance().draw_rand()*0.5;
 				if(albedo > 1.0){
 					albedo = 1.0 - GenRandomRolls.Instance().draw_rand() * 0.01;
 				}
+				logger.info("end albedo:"+albedo);
 				return albedo;
 			}
 		});
 		temperatureMap.put(new Double(150.0), new PlanarTemperature() {
 			@Override
 			public Double modAlbedo(Double albedo) {
+				logger.info("start albedo:"+albedo);
 				albedo += GenRandomRolls.Instance().draw_rand()*0.2;
 				if(albedo > 1.0){
 					albedo = 1.0 - GenRandomRolls.Instance().draw_rand() * 0.01;
 				}
+				logger.info("end albedo:"+albedo);
 				return albedo;
 			}
 		});
@@ -64,55 +77,67 @@ public class PlanarAlbedo {
 		temperatureMap.put(new Double(400.0), new PlanarTemperature() {
 			@Override
 			public Double modAlbedo(Double albedo) {
+				logger.info("start albedo:"+albedo);
 				albedo += GenRandomRolls.Instance().draw_rand()*0.2;
 				if(albedo > 1.0){
 					albedo = 1.0 - GenRandomRolls.Instance().draw_rand() * 0.01;
 				}
+				logger.info("end albedo:"+albedo);
 				return albedo;
 			}
 		});
 		temperatureMap.put(new Double(600.0), new PlanarTemperature() {
 			@Override
 			public Double modAlbedo(Double albedo) {
+				logger.info("start albedo:"+albedo);
 				albedo += GenRandomRolls.Instance().draw_rand()*0.4;
 				if(albedo > 1.0){
 					albedo = 1.0 - GenRandomRolls.Instance().draw_rand() * 0.01;
 				}
+				logger.info("end albedo:"+albedo);
 				return albedo;
 			}
 		});
 		temperatureMap.put(new Double(1000.0), new PlanarTemperature() {
 			@Override
 			public Double modAlbedo(Double albedo) {
+				logger.info("start albedo:"+albedo);
 				albedo += GenRandomRolls.Instance().draw_rand()*0.7;
 				if(albedo > 1.0){
 					albedo = 1.0 - GenRandomRolls.Instance().draw_rand() * 0.01;
 				}
+				logger.info("end albedo:"+albedo);
 				return albedo;
 			}
 		});
 		temperatureMap.put(Double.MAX_VALUE, new PlanarTemperature() {
 			@Override
 			public Double modAlbedo(Double albedo) {
+				logger.info("start albedo:"+albedo);
 				albedo += GenRandomRolls.Instance().draw_rand()*0.8;
 				if(albedo > 1.0){
 					albedo = 1.0 - GenRandomRolls.Instance().draw_rand() * 0.01;
 				}
+				logger.info("end albedo:"+albedo);
 				return albedo;
 			}
 		});
 	}
-	
+	/**
+	 * method does not account for absorption and reflection for individual chemical elements at light frequencies.
+	 */
 	private static Map<String, PlanarAtmosphere> atmosphereMap = new HashMap<String, PlanarAtmosphere>();
 	static{
 		atmosphereMap.put("Chlorine", new PlanarAtmosphere() {
 			
 			@Override
 			public Double modAlbedo(Double albedo, Double percentage) {
+				logger.info("start albedo:"+albedo);
 				albedo -= (percentage * GenRandomRolls.Instance().draw_rand() * 0.2 );
 				if(albedo < 0.0){
 					albedo = 0.0 + GenRandomRolls.Instance().draw_rand() * 0.01;
 				}
+				logger.info("end albedo:"+albedo);
 				return albedo;
 			}
 		});
@@ -120,10 +145,12 @@ public class PlanarAlbedo {
 			
 			@Override
 			public Double modAlbedo(Double albedo, Double percentage) {
+				logger.info("start albedo:"+albedo);
 				albedo -= (percentage * GenRandomRolls.Instance().draw_rand() * 0.3 );
 				if(albedo < 0.0){
 					albedo = 0.0  + GenRandomRolls.Instance().draw_rand() * 0.01;
 				}
+				logger.info("end albedo:"+albedo);
 				return albedo;
 			}
 		});
@@ -131,10 +158,12 @@ public class PlanarAlbedo {
 			
 			@Override
 			public Double modAlbedo(Double albedo, Double percentage) {
+				logger.info("start albedo:"+albedo);
 				albedo -= (percentage * GenRandomRolls.Instance().draw_rand() * 0.1 );
 				if(albedo < 0.0){
 					albedo = 0.0 + GenRandomRolls.Instance().draw_rand() * 0.01;
 				}
+				logger.info("end albedo:"+albedo);
 				return albedo;
 			}
 		});
@@ -146,30 +175,36 @@ public class PlanarAlbedo {
 		sizeMap.put(new Double(0.0), new PlanarSize() {
 			@Override
 			public Double modAlbedo(Double albedo) {
+				logger.info("start albedo:"+albedo);
 				albedo -= (GenRandomRolls.Instance().draw_rand() * 0.9);
 				if(albedo < 0.0){
 					albedo = 0.0 + GenRandomRolls.Instance().draw_rand() * 0.01;
 				}
+				logger.info("end albedo:"+albedo);
 				return albedo;
 			}
 		});
 		sizeMap.put(new Double(1000.0), new PlanarSize() {
 			@Override
 			public Double modAlbedo(Double albedo) {
+				logger.info("start albedo:"+albedo);
 				albedo -= (GenRandomRolls.Instance().draw_rand() * 0.7);
 				if(albedo < 0.0){
 					albedo = 0.0 + GenRandomRolls.Instance().draw_rand() * 0.01;
 				}
-				return albedo;
+				logger.info("end albedo:"+albedo);
+			return albedo;
 			}
 		});
 		sizeMap.put(new Double(1500.0), new PlanarSize() {
 			@Override
 			public Double modAlbedo(Double albedo) {
+				logger.info("start albedo:"+albedo);
 				albedo -= (GenRandomRolls.Instance().draw_rand() * 0.3);
 				if(albedo < 0.0){
 					albedo = 0.0 + GenRandomRolls.Instance().draw_rand() * 0.01;
 				}
+				logger.info("end albedo:"+albedo);
 				return albedo;
 			}
 		});
@@ -182,26 +217,30 @@ public class PlanarAlbedo {
 		sizeMap.put(new Double(10000.0), new PlanarSize() {
 			@Override
 			public Double modAlbedo(Double albedo) {
+				logger.info("start albedo:"+albedo);
 				albedo += (GenRandomRolls.Instance().draw_rand() * 0.2);
 				if(albedo > 1.0){
 					albedo = 1.0 + GenRandomRolls.Instance().draw_rand() * 0.01;
 				}
+				logger.info("end albedo:"+albedo);
 				return albedo;
 			}
 		});
 		sizeMap.put(Double.MAX_VALUE, new PlanarSize() {
 			@Override
 			public Double modAlbedo(Double albedo) {
+				logger.info("start albedo:"+albedo);
 				albedo += (GenRandomRolls.Instance().draw_rand() * 0.2);
 				if(albedo > 1.0){
 					albedo = 1.0 + GenRandomRolls.Instance().draw_rand() * 0.04;
 				}
+				logger.info("end albedo:"+albedo);
 				return albedo;
 			}
 		});
 	}
 	
-	public static Double typicalAlbedo = 30.0;
+	public static Double typicalAlbedo = 0.30;
 	
 	
 	
@@ -216,22 +255,40 @@ public class PlanarAlbedo {
 	 * @param unifiedPlanetoidI
 	 * @return generated albedo
 	 */
-	public static Double genAlbedoPlanar(UnifiedPlanetoidI unifiedPlanetoidI){
+	public static Double genAlbedoPlanar(UnifiedPlanetoidI unifiedPlanetoidI) {
 		Double startAlbedo = typicalAlbedo;
 		Set<Double> temperatureMapKeys = temperatureMap.keySet();
-		Iterator<Double> temperatureIterator = temperatureMapKeys.iterator(); 
-		Double temperatureScalar = null;
+		Iterator<Double> temperatureIterator = temperatureMapKeys.iterator();
 		Double currentTemperatureKey = temperatureIterator.next();
 		while (temperatureIterator.hasNext()) {
 			Double nextTemperatureKey = temperatureIterator.next();
 			if (unifiedPlanetoidI.getPlanetoid().getTemperature() > currentTemperatureKey
 					&& unifiedPlanetoidI.getPlanetoid().getTemperature() <= nextTemperatureKey) {
-				temperatureScalar = temperatureMap.get(nextTemperatureKey).modAlbedo(startAlbedo);
+				startAlbedo = temperatureMap.get(nextTemperatureKey).modAlbedo(startAlbedo);
 				break;
 			}
-			
+			currentTemperatureKey = nextTemperatureKey;
 		}
-
+		List<Atmosphere> atmospheres = null;
+		atmospheres = ExistingSystemWithStars.readPlanarsAtmosphere(unifiedPlanetoidI.getPlanetoid());
+		for (Atmosphere atmosphere : atmospheres) {
+			if (atmosphereKeys.contains(atmosphere.getChem_name())) {
+				startAlbedo = atmosphereMap.get(atmosphere.getChem_name()).modAlbedo(startAlbedo,
+						atmosphere.getPercentage());
+			}
+		}
+		Set<Double> sizeMapKeys = sizeMap.keySet();
+		Iterator<Double> sizeIterator = sizeMapKeys.iterator();
+		Double currentSizeKey = sizeIterator.next();
+		while(sizeIterator.hasNext()){
+			Double nextSizeKey = sizeIterator.next();
+			if(unifiedPlanetoidI.getPlanetoid().getRadius() > currentSizeKey &&
+					unifiedPlanetoidI.getPlanetoid().getRepId() <= nextSizeKey){
+				startAlbedo = sizeMap.get(nextSizeKey).modAlbedo(startAlbedo);
+				break;
+			}
+			currentSizeKey = nextSizeKey;
+		}
 		return startAlbedo;
 	}
 	
